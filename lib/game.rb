76f1e5ae413d139e10e9639561271948087aea49
Joseph Combs <joseph.e.combs@gmail.com>
2014-08-15 18:06:24 -0700

    Need to come back and finish the game class

--- lib/game.rb
+++ lib/game.rb
@@ -14,15 +14,38 @@ class Game
     counter = 0
     player_arr = [@player1,@player2]
     while @player1.bankroll > 0 && @player2.bankroll > 0 
-      p "player1 hand: #{@player1.hand}"
+      p "player1 hand: #{@player1.hand.cards_array}"
+      p "player2 hand: #{@player2.hand.cards_array}"
       toggle = player_arr[counter % 2]
       
+      betting = true
+      while betting
+        betting, raised = parse_input(player_arr[toggle].get_bet)
+      end
+      
+      @player1.get_recycle_cards
+      @player2.get_recycle_cards
+      
+      betting = true
+      while betting
+        
+        
+      end
       
       p "AAAAA"
       gets.chomp
     end
   end
   
+  def parse_input(arg, bet_size)
+    if arg == r
+      
+    elsif arg == c
+      
+    else
+      @pot += bet_size
+    end
+  end
 end
 
 a = Game.new
